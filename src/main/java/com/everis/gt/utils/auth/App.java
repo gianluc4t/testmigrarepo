1a81d7149ed075ccefd6514ece5b1d45b4400a34
Gianluca Turano <giturano@everis.com>
2019-02-15 17:29:35 -0200

    init

---
+++ src/main/java/com/everis/gt/utils/auth/App.java
@@ -0,0 +1,12 @@
+package com.everis.gt.utils.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(App.class, args);
+    }
+}
